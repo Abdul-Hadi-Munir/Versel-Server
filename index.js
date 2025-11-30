324a091f26b874e9e5e07f8d312545d737f41c73
ABDUL HADI MUNIR <ahmpk111@gmail.com>
2025-12-01 02:51:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import cors from "cors";
+
+app.use(cors({
+  origin: "https://versel-client-black.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -30,3 +39,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
